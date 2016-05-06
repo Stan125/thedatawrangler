46a7d678ce7fb0373039d06f42a574168454408f
Stan125 <stanislaus@stadlmann.cm>
2016-05-06 12:50:49 +0200

    Working reactiveness
    - Introduced general archive for the preview

--- thedatawrangler.R
+++ thedatawrangler.R
@@ -7,7 +7,6 @@
 #.............................
 # PRELIMINARIES
 #.............................
-
 library(rstudioapi)
 library(dplyr)
 library(miniUI)
@@ -48,37 +47,34 @@ ui <- miniPage(
   miniTabstripPanel(
     miniTabPanel("Data", icon = icon("folder-open"),
                  miniContentPanel(
-                   fillCol(
-                     fillCol(
                        selectInput(label = "Select your dataset:",
                                    inputId = "dataset",
                                    choices = c("", search_df())),
                        actionButton("update_df", "Update dataframe",
                                     class = "btn btn-primary btn-sm action-button",
-                                    style = "float:left")
-                     ),
-                     fillCol(
-                       selectInput("group",
-                                   "Select variable for grouping:",
-                                   choices = c("A categorial variable")),
+                                    style = "float:left"),
+                       br(),
+                       br(),
+                       br(),
+                       h5("Perform grouping after selecting dataframe"),
+                       uiOutput("variables"),
                        actionButton("update_group", "Perform grouping",
                                     class = "btn btn-primary btn-sm action-button",
                                     style = "float:left")
-                     )
-                   )
                  )
     ),
     miniTabPanel("Reshape", icon = icon("exchange")
                  # Content Tab 2
     ),
     miniTabPanel("Subset", icon = icon("scissors")
-                   # Content Tab 3
+                 # Content Tab 3
     ),
     miniTabPanel("Summarise", icon = icon("compress")
                  # Content Tab 4
     ),
     miniTabPanel("Preview", icon = icon("table"),
                  DT::dataTableOutput("previewtable")
+                 #textOutput("tableprint")
     )
   )
 )
@@ -86,41 +82,49 @@ ui <- miniPage(
 server <- function(input, output, session) {
   
   # Start with iris DF
-  preview <- data.frame(iris)
+  values <- reactiveValues()
   
-  # Check whether dataset was chosen
-  dataset <- reactive({validate(
-    need(input$dataset != "", "Please pick a dataset")
-  )
-    input$dataset
+  # If df button is pressed update dataframe
+  observeEvent(input$update_df, {
+    values$preview <- tbl_df(get(input$dataset))
+    cat("Dataset button was pressed\n")
   })
   
-  # If button is pressed update dataframe
-  preview <- eventReactive(input$update_df, {
-    get(dataset())
+  # If group_by button is pressed group and update dataframe
+  observeEvent(input$update_group, {
+    values$preview <- group_by_(values$preview, input$group_variable)
+    cat("Group_by button was pressed \n")
+  })
+  
+  # Render the UI button with variable selections, if dataset is selected
+  output$variables <- renderUI({
+    col.names <- colnames(values$preview)
+    selectInput(inputId = "group_variable",
+                label = "Select your variable",
+                choices = col.names)
   })
   
   # Render Preview table
   output$previewtable <-  DT::renderDataTable({
-      preview()
+    as.data.frame(values$preview)
     }, options = list(autoWidth = FALSE, 
                       paging = TRUE,
                       searching = FALSE,
-                      info = FALSE,
-                      ordering = TRUE,
+                      info = TRUE,
+                      ordering = FALSE,
                       processing = FALSE,
                       scrollX = TRUE),
     class = "cell-border stripe")
   
+  # Do this when pressing "Close"
   observeEvent(input$done, {
-    
     stopApp()
   })
 }
 
 # We'll use a pane viwer, and set the minimum height at
 # 300px to ensure we get enough screen space to display the clock.
-viewer <- dialogViewer(dialogName = "The Data Wrangler", width = 600, height = 600)
+viewer <- dialogViewer(dialogName = "The Data Wrangler", width = 700, height = 700)
 runGadget(ui, server, viewer = viewer)
 
 
